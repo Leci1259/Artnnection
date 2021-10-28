cb3e21a9646a637cfdef5f6bbdf13d407adfb7fb
Leci1259 <lreid31@gatech.edu>
2021-10-28 12:20:03 -0400

    worked on cart and artist js files

--- public/js/cart.js
+++ public/js/cart.js
@@ -7,22 +7,53 @@ const searchBarHandler = async (event) => {
     if ( searchInput && (searchCategory == 'Artist')) {
         const response = await fetch('api/user/', {
             method: 'GET',
-            body: JSON.stringify({}),
         })
+
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            document.location.replace('/user/checkout');
+          } else {
+            alert(response.statusText);
+          }
     }
 
     if ( searchInput && (searchCategory == 'Art')) {
         const response = await fetch('api/art/', {
             method: 'GET',
-            body: JSON.stringify({}),
         })
+
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            document.location.replace('/user/checkout');
+          } else {
+            alert(response.statusText);
+          }
     }
 }
 
 const addToCartHandler = async (event) => {
-    const art = event.target;
+    
+    const artName = event.target.sibling(2).firstChild().value.trim();
+    const artPrice= event.target.sibling(2).children(2).value.trim();
+    const buyOption =event.target.sibling(3).value;
 
-    const buyOption = document.querySelector('.buy-option').value.trim();
+    //use event.target to grab values from that card
+
+    // const buyOption = document.querySelector('.buy-option').value.trim();
+    // const artName = document.querySelector('#art-title').value.trim();
+    // const artPrice= document.querySelector('#art-price').value.trim();
+
+    const response = await fetch('api/', {
+        method: 'POST',
+        body: JSON.stringify({buyOption,artName,artPrice}),
+    })
+
+    if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/user/checkout');
+      } else {
+        alert(response.statusText);
+      }
 
 }
 
@@ -30,7 +61,6 @@ const addToCartHandler = async (event) => {
 document
   .querySelector('.searchbutton')
   .addEventListener('click', searchBarHandler);
-
-  document
+   document
   .querySelector('.cartbutton')
   .addEventListener('click', addToCartHandler);
