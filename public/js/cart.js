ff4301afd6ac7a4b4f2b8673d1410c891cd3b400
Leci1259 <lreid31@gatech.edu>
2021-10-28 11:01:58 -0400

    adjusting handlebars for js codes

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,36 @@
-const searchBarhandler = async (event) => {
+const searchBarHandler = async (event) => {
     event.preventDefault();
 
     const searchInput = document.querySelector('#search-input').value.trim();
-    const searchCategory = document.querySelector()
+    const searchCategory = document.querySelector('.search-category').value.trim();
+
+    if ( searchInput && (searchCategory == 'Artist')) {
+        const response = await fetch('api/user/', {
+            method: 'GET',
+            body: JSON.stringify({}),
+        })
+    }
+
+    if ( searchInput && (searchCategory == 'Art')) {
+        const response = await fetch('api/art/', {
+            method: 'GET',
+            body: JSON.stringify({}),
+        })
+    }
 }
+
+const addToCartHandler = async (event) => {
+    const art = event.target;
+
+    const buyOption = document.querySelector('.buy-option').value.trim();
+
+}
+
+
+document
+  .querySelector('.searchbutton')
+  .addEventListener('click', searchBarHandler);
+
+  document
+  .querySelector('.cartbutton')
+  .addEventListener('click', addToCartHandler);
