79f1bb3c9b3d38628f264f912a82a6572825807a
Leci1259 <lreid31@gatech.edu>
2021-10-28 10:18:31 -0400

    adding cart js file

---
+++ public/js/cart.js
@@ -0,0 +1,6 @@
+const searchBarhandler = async (event) => {
+    event.preventDefault();
+
+    const searchInput = document.querySelector('#search-input').value.trim();
+    const searchCategory = document.querySelector()
+}
