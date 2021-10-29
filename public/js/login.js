b68b86f97e268a6daa2738da999243c24333e6dc
Leci1259 <lreid31@gatech.edu>
2021-10-29 11:39:00 -0400

    removed new npm stuff

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,13 @@
-const { passwordStrength } = require('check-password-strength');
-
 
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  // Collect values from the login form
+  //Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    // Send a POST request to the API endpoint
+    //Send a POST request to the API endpoint
     const response = await fetch('/api/user/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -17,7 +15,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
+     // If successful, redirect the browser to the profile page
       document.location.replace('/user_profile');
     } else {
       alert(response.statusText);
@@ -32,12 +30,6 @@ const signupFormHandler = async (event) => {
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  const passText= passwordStrength(password).value;
-  $('#passStrength').text(passText)
-  if (passText !== 'Strong') {
-    $('#passParam').text("Password not strong enough. Please have at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character, and have a length of at least 8.")
-  }
-  else{
   if (name && email && password) {
     const response = await fetch('/api/user/signup', {
       method: 'POST',
@@ -51,7 +43,7 @@ const signupFormHandler = async (event) => {
       alert(response.statusText);
     }
   }
-}
+
 };
 
 
