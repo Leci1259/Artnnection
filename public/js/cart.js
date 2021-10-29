c8564e923d3efec7cbc3b5e0c1f1a59ad037ac74
Leci1259 <lreid31@gatech.edu>
2021-10-29 13:04:22 -0400

    set up local storage for cart and checkout page rendering of cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,4 +1,8 @@
-
+var cart={
+  name:[],
+  price:[],
+  option:[],
+}
 
 const addToCartHandler = async (event) => {
      // use event.target to grab values from that card
@@ -8,19 +12,17 @@ const addToCartHandler = async (event) => {
     const artPrice= $(button).siblings().eq(1).children().eq(2).text();
     const buyOption =$(button).siblings().eq(2).val();
 
-    const response = await fetch('api/', {
-        method: 'POST',
-        body: JSON.stringify({buyOption,artName,artPrice}),
-    })
+    cart.name.push(artName);
+    cart.price.push(artPrice);
+    cart.option.push(buyOption);
 
-    if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/user/checkout');
-      } else {
-        alert(response.statusText);
-      }
+    localStorage.setItem('cart', JSON.stringify(cart));
 
 }
+
+
+
+
 
 
 document
