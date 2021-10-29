2f6230ff5f5b6fa7701d1b6bea63d9bf9389518d
Leci1259 <lreid31@gatech.edu>
2021-10-29 13:44:02 -0400

    connected js files to intended handlebars

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const routes = require("./routes");
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
 const { dirname } = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: ".hbs",
+  helpers: {
+    section: function(name, options) { 
+      if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this); 
+        return null;
+      }
+  }    
+});
 
 const sess = {
   secret: 'Super secret secret',
@@ -22,8 +31,8 @@ const sess = {
 app.use(session(sess));
 
 //Express.js handlebar connection
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
